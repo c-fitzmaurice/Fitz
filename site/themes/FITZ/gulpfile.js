55791be39f2e4d211f43ee3fdc3d50b501a79a52
Colin Fitz-Maurice <colin@fitz-maurice.com>
2016-09-27 21:01:14 -0400

    v1 archive and npm update

--- site/themes/FITZ/gulpfile.js
+++ site/themes/FITZ/gulpfile.js
@@ -4,19 +4,10 @@ var theme = 'fitz';
 elixir.config.assetsPath = './';
 elixir.config.publicPath = './';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Statamic theme. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass(theme + '.scss', 'css/' + theme + '.css');
+    mix
+
+    .sass(theme + '.scss', 'css/' + theme + '.css');
 
     // mix.version('css/' + theme + '.css');
 });
