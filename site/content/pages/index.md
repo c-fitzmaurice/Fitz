61d6596654b5a56c655e3cd1b43a9a32cb2926cc
Colin Fitz-Maurice <colin@fitz-maurice.com>
2016-10-02 22:49:08 -0400

    Think I have a homepage

--- site/content/pages/index.md
+++ site/content/pages/index.md
@@ -7,4 +7,4 @@ list:
   - here
 id: db0ae4e3-4f10-4802-bc40-0b880cbf02c7
 ---
-Hello. I'm Colin Fitz-Maurice, a [Laravel](https://laravel.com/) Developer with a Frontend Focus, working for [@Goodworld](https://goodworld.me/) and living in Philadelphia.
+Hello. I'm Colin Fitz-Maurice, a <a href="https://laravel.com/" target="_blank">Laravel</a> Developer with a Frontend Focus, working for <a href="#" data-toggle="tooltip" title="Some tooltip text!">@Goodworld</a> and living in Philadelphia.
--- site/themes/fitz/js/fitz.js
+++ site/themes/fitz/js/fitz.js
@@ -1,36 +1,4 @@
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
-//# sourceMappingURL=fitz.js.map
-
+$(document).ready(function(){$("#menulink").click(function(a){a.preventDefault(),$(".navigation-wrapper").hasClass("show-menu")?($(".navigation-wrapper").removeClass("show-menu"),$(".navigation").hide(),$(".navigation li").removeClass("small-padding")):($(".navigation-wrapper").addClass("show-menu"),$(".navigation").fadeIn(),$(".navigation li").addClass("small-padding"))})});
 //# sourceMappingURL=fitz.js.map
 
 //# sourceMappingURL=fitz.js.map
