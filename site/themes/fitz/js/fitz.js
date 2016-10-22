a280199631027a732c7b2babfb35e2616c31086a
Colin Fitz-Maurice <colin@fitz-maurice.com>
2016-10-22 13:21:01 -0400

    New menu, 404 page, social graph images and new favicon

--- site/themes/fitz/js/fitz.js
+++ site/themes/fitz/js/fitz.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
 
-    $("#menulink").click(function (a) {
-        a.preventDefault(),
-        $(".navigation-wrapper").hasClass("show-menu") ?
-            (
-                $(".navigation-wrapper").removeClass("show-menu"),
-                $(".navigation").hide(),
-                $(".navigation li").removeClass("small-padding")
-            )
-        :
-            (
-                $(".navigation-wrapper").addClass("show-menu").fadeIn(),
-                $(".navigation").fadeIn(),
-                $(".navigation li").addClass("small-padding")
-            )
-    })
+    $("#menu-x").click(function (a) {
+        if($("#menu-x").is(':checked')) {
+            $(".navigation-wrapper").addClass("show-menu").fadeIn();
+            $(".navigation").fadeIn();
+            $(".navigation li").addClass("small-padding");
+        } else {
+            $(".navigation-wrapper").removeClass("show-menu");
+            $(".navigation").hide();
+            $(".navigation li").removeClass("small-padding");
+        }
+    });
 
 });
 
