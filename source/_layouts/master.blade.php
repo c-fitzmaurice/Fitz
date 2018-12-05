fce5f4bf306180a82cebace8632b2310847f46b0
Colin <colin@fitz-maurice.com>
2018-12-05 15:48:55 -0500

    Set up tailwind

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="bg-grey font-sans">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,7 +8,14 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body>
-        @yield('body')
+    <body class="bg-white border-solid border-t-4 border-black flex flex-col min-h-screen">
+        {{-- @include('_partials.menu') --}}
+        <div class="container">
+            @yield('body')
+        </div>
+        {{-- @include('_partials.footer') --}}
+        {{-- <script src="/js/manifest.js"></script> --}}
+        {{-- <script src="/js/vendor.js"></script> --}}
+        {{-- <script src="/js/main.js"></script> --}}
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,34 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
+require('laravel-mix-purgecss');
+require('laravel-mix-tailwind');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-    ]
+  plugins: [
+    build.jigsaw,
+    build.browserSync(),
+    build.watch([
+      'source/**/*.md',
+      'source/**/*.php',
+      'source/**/*.scss',
+      '!source/**/_tmp/*'
+    ])
+  ]
 });
 
-mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
-    .options({
-        processCssUrls: false,
-    }).version();
+mix
+  .js('source/_assets/js/main.js', 'js')
+  .sass('source/_assets/sass/main.scss', 'css')
+  .tailwind()
+  .purgeCss({
+    globs: [
+      path.join(__dirname, 'source/**/*.blade.php'),
+      path.join(__dirname, 'source/_assets/**/*.blade.php')
+    ]
+  })
+  .options({
+    processCssUrls: false
+  })
+  .version();
