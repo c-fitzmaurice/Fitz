f4a8b368f9546a29099ce8cc1e0b4ad03c74c79d
Colin <colin@fitz-maurice.com>
2019-04-10 02:34:20 -0400

    final update

--- statamic/vendor/autoload.php
+++ statamic/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0e2a1c61d40ac6c0e79762b6c7388d79::getLoader();
+return ComposerAutoloaderInit3e3ff943617c52ef1b04c2aed2c0ac67::getLoader();
